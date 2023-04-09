c03a122b651cd5fedfa5eebc9bbc412611fab2eb
gustavo rocha <pereiragustavo215@gmail.com>
2023-04-09 15:35:50 -0300

    criando alguns testes

---
+++ app/src/androidTest/java/br/com/alura/panucci/NavigationTest.kt
@@ -0,0 +1,84 @@
+package br.com.alura.panucci
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import br.com.alura.panucci.navigation.drinksRoute
+import br.com.alura.panucci.navigation.highlightsListRoute
+import br.com.alura.panucci.navigation.menuRoute
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+
+class NavigationTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupAppNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            PanucciApp(navController = navController)
+        }
+    }
+
+    @Test
+    fun appNavHost_verifyStartDestination() {
+        // log para ver os nós da arvore do compose
+        composeTestRule.onRoot().printToLog("panucci app")
+
+        composeTestRule.onNodeWithText("Destaques do dia")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun appNavHost_verifyIfMenuScreenIsDisplayed() {
+        composeTestRule.onRoot().printToLog("panucci app")
+        composeTestRule.onNodeWithText("Menu")
+            .performClick()
+
+        composeTestRule.onAllNodesWithText("Menu")
+            .assertCountEquals(2)
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, menuRoute)
+    }
+
+    @Test
+    fun appNavHost_verifyIfDrinksScreenIsDisplayed() {
+        composeTestRule.onRoot().printToLog("panucci app")
+        composeTestRule.onNodeWithText("Bebidas")
+            .performClick()
+
+        composeTestRule.onAllNodesWithText("Bebidas")
+            .assertCountEquals(2)
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, drinksRoute)
+    }
+
+    @Test
+    fun appNavHost_verifyIHighlightsScreenIsDisplayed() {
+        composeTestRule.onRoot().printToLog("panucci app")
+        composeTestRule.onNodeWithText("Destaques")
+            .performClick()
+
+        composeTestRule.onAllNodesWithText("Destaques do dia")
+            .assertCountEquals(1)
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, highlightsListRoute)
+    }
+}
--- app/src/main/java/br/com/alura/panucci/MainActivity.kt
+++ app/src/main/java/br/com/alura/panucci/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.datastore.preferences.core.edit
 import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import br.com.alura.panucci.navigation.PanucciNavHost
@@ -37,92 +39,98 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val context = LocalContext.current
-            val navController = rememberNavController()
-            val scope = rememberCoroutineScope()
-            val backStackEntryState by navController.currentBackStackEntryAsState()
-            val currentDestination = backStackEntryState?.destination
-
-            val orderDoneMessage =
-                backStackEntryState?.savedStateHandle
-                    ?.getStateFlow<String?>("order_done", null)
-                    ?.collectAsState()
-
-            val snackbarHostState = remember {
-                SnackbarHostState()
-            }
-
-            orderDoneMessage?.value?.let { message ->
-                scope.launch {
-                    snackbarHostState.showSnackbar(message = message)
-                }
-            }
-
-            //Use o LauccheDEffect para logica que não se aplica a click ou eventos
-            // Pq o compose executa a criacao de tela com muita frequencia e se essa logica não for
-            // tratada entrarar em loop de navegacao
-            /*LaunchedEffect(key1 = Unit){
-                navController.navigate("menu")
-            }*/
-
-            // Considerando que é uma ação que pode ser afetada pela recomposição,
-            // precisamos realizar a navegação utilizando a API de Effect
-            // ou a partir de um evento de um composable que utilizar a API de Effect internamente.
-
             PanucciTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    val currentRoute = currentDestination?.route
-                    val selectedItem by remember(currentDestination) {
-                        val item = when (currentRoute) {
-                            highlightsListRoute -> HighlightsList
-                            menuRoute -> Menu
-                            drinksRoute -> Drinks
-                            else -> HighlightsList
-                        }
-                        mutableStateOf(item)
-                    }
-
-                    val containsInBottomAppBarItems = when (currentRoute) {
-                        highlightsListRoute, menuRoute, drinksRoute -> true
-                        else -> false
-                    }
-
-                    val isShowFab = when (currentRoute) {
-                        menuRoute, drinksRoute -> true
-                        else -> false
-                    }
-
-                    PanucciAppScreen(
-                        snackbarHostState = snackbarHostState,
-                        bottomAppBarItemSelected = selectedItem,
-                        onBottomAppBarItemSelectedChange = { item ->
-                            navController.navigateSingleTopWithPopUpTo(item)
-                        },
-                        onFabClick = { navController.navigateToCheckout() },
-                        isShowTopBar = containsInBottomAppBarItems,
-                        isShowBottomBar = containsInBottomAppBarItems,
-                        isShowFab = isShowFab,
-                        onLogout = {
-                            scope.launch {
-                                context.dataStore.edit {
-                                    it.remove(userPreferences)
-                                }
-                            }
-
-                            navController.navigateToAuthentication {
-                                popUpTo(navController.graph.findStartDestination().id) {
-                                    inclusive = true
-                                }
-                            }
-                        }
-                    ) {
-                        PanucciNavHost(navController = navController)
-                    }
+                    PanucciApp()
                 }
             }
         }
     }
+}
 
+@Composable
+fun PanucciApp(navController: NavHostController = rememberNavController()) {
+
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val backStackEntryState by navController.currentBackStackEntryAsState()
+    val currentDestination = backStackEntryState?.destination
+
+    val orderDoneMessage =
+        backStackEntryState?.savedStateHandle
+            ?.getStateFlow<String?>("order_done", null)
+            ?.collectAsState()
+
+    val snackbarHostState = remember {
+        SnackbarHostState()
+    }
+
+    orderDoneMessage?.value?.let { message ->
+        scope.launch {
+            snackbarHostState.showSnackbar(message = message)
+        }
+    }
+
+    //Use o LauccheDEffect para logica que não se aplica a click ou eventos
+    // Pq o compose executa a criacao de tela com muita frequencia e se essa logica não for
+    // tratada entrarar em loop de navegacao
+    /*LaunchedEffect(key1 = Unit){
+        navController.navigate("menu")
+    }*/
+
+    // Considerando que é uma ação que pode ser afetada pela recomposição,
+    // precisamos realizar a navegação utilizando a API de Effect
+    // ou a partir de um evento de um composable que utilizar a API de Effect internamente.
+
+
+    val currentRoute = currentDestination?.route
+    val selectedItem by remember(currentDestination) {
+        val item = when (currentRoute) {
+            highlightsListRoute -> HighlightsList
+            menuRoute -> Menu
+            drinksRoute -> Drinks
+            else -> HighlightsList
+        }
+        mutableStateOf(item)
+    }
+
+    val containsInBottomAppBarItems = when (currentRoute) {
+        highlightsListRoute, menuRoute, drinksRoute -> true
+        else -> false
+    }
+
+    val isShowFab = when (currentRoute) {
+        menuRoute, drinksRoute -> true
+        else -> false
+    }
+
+    PanucciAppScreen(
+        snackbarHostState = snackbarHostState,
+        bottomAppBarItemSelected = selectedItem,
+        onBottomAppBarItemSelectedChange = { item ->
+            navController.navigateSingleTopWithPopUpTo(item)
+        },
+        onFabClick = { navController.navigateToCheckout() },
+        isShowTopBar = containsInBottomAppBarItems,
+        isShowBottomBar = containsInBottomAppBarItems,
+        isShowFab = isShowFab,
+        onLogout = {
+            scope.launch {
+                context.dataStore.edit {
+                    it.remove(userPreferences)
+                }
+            }
+
+            navController.navigateToAuthentication {
+                popUpTo(navController.graph.findStartDestination().id) {
+                    inclusive = true
+                }
+            }
+        }
+    ) {
+        PanucciNavHost(navController = navController)
+    }
 }
